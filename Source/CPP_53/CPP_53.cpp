028fd41d4f66f31600b974bc5d307ebb7bad36d9
nathanjoly7 <nathanjoly77@gmail.com>
2024-11-23 16:50:08 +0100

    THANOS commit
    J'ai sacrifié la moitié du projet pour que l'autre puisse vivre en paix.
    
    Suppression de tout le cpp, suppression de la solution, suppression de la section modules dans le code du uproject. C'était le seul moyen de pouvoir lancer le projet.

--- Source/CPP_53/CPP_53.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CPP_53.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPP_53, "CPP_53" );
--- Source/CPP_53/MemoryGM.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "MemoryGM.h"
-
-void AMemoryGM::UpdateScore(int32 value)
-{
-	MemoryScore = MemoryScore + value;
-}
